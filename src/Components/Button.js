0bd3f4955c071e58da6b0f62d7a118784e19a1c0
liuchang5454 <liuchang5454@gmail.com>
2018-11-03 00:08:06 -0600

    create folder structure as Components and Logic, add 2 components

---
+++ src/Components/Button.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Button extends Component {
+  render(){
+    return (
+      <div className="ButtonPanel">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default Button;
---
+++ src/Components/ButtonPanel.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Button from './Button';
+
+class ButtonPanel extends Component {
+  render(){
+    return (
+      <div className="ButtonPanel">
+        <Button />
+      </div>
+    );
+  }
+}
+
+export default ButtonPanel;
